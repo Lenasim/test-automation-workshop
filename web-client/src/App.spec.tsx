2491c671e42e388ec1aa2a7002718c14e8c75515
LenaSim <radical.rin@gmail.com>
2020-12-16 10:29:25 +0100

    Bug fixed

--- web-client/src/App.spec.tsx
+++ web-client/src/App.spec.tsx
@@ -1,99 +1,71 @@
-import React from 'react'
-import {render, screen, waitFor, within} from '@testing-library/react'
-import App, {GET_WILDERS} from './App'
-import { MockedProvider } from '@apollo/client/testing'
-import { GraphQLError } from 'graphql';
+import { MockedProvider } from '@apollo/client/testing';
+import { render, screen, waitFor, within } from '@testing-library/react';
+import React from 'react';
+
+import App, { GET_WILDERS } from './App';
+
+const GET_WILDERS_SUCCESS_MOCK = {
+  request: {
+    query: GET_WILDERS,
+  },
+  result: {
+    data: {
+      wilders: [
+        {
+          id: '1',
+          displayName: '[SXB] Lorie Laurette',
+        },
+        {
+          id: '2',
+          displayName: '[PAR] Laure Laurent',
+        },
+      ],
+    },
+  },
+};
 
 
-const GET_WILDERS_UNFETCHED_MOCK = []
-//fictive data
-const GET_WILDERS_SUCCESS_MOCK =[ 
-  {
-    request: {
-      query: GET_WILDERS,
-    },
-    result: {
-      data: {
-        wilders: [
-          {
-            id: '1',
-            displayName: '[SXB] Lorie Laurette',
-          },
-          {
-            id: '2',
-            displayName: '[PAR] Laure Laurent',
-          },
-        ],
-      },
-    },
+const GET_WILDERS_ERROR_MOCK = {
+  request: {
+    query: GET_WILDERS,
+  },
+  result: { 
+    errors: [{ message: "An error occurred" }] 
   }
-];
+};
 
-const apolloError = new GraphQLError('Error!');
-
-
-const GET_WILDERS_ERROR_MOCK = [
-  {
-    request: {
-      query: GET_WILDERS,
-    },
-
-    // result: {
-    //   errors: [apolloError],
-    // },
-
-    // result: {
-    //   errors: [new GraphQLError('Error!')],
-    // },
-
-    // error: new Error('An error occurred'),
-
-    // result: { 
-    //   errors: [{ message: "An error occurre" }] 
-    // }
-
-    error: {
-      name: 'GRAPHQL_VALIDATION_FAILED',
-      message:
-        'Cannot query field "wilders_error" on type "Query". Did you mean "wilders"?',
-    },
-  }
-]
-
-
-describe('App',() => {
-
+describe('App', () => {
   describe('while fetching wilders', () => {
     it('renders loading', () => {
       render(
-        <MockedProvider mocks={GET_WILDERS_UNFETCHED_MOCK} addTypename={false}>
+        <MockedProvider mocks={[GET_WILDERS_SUCCESS_MOCK]} addTypename={false}>
           <App />
         </MockedProvider>
-        )
-      expect(screen.getByText('Chargement en cours…')).toBeInTheDocument()
-    })
-  })
+      );
 
-  describe('while fetching wilders failed', () => {
+      expect(screen.getByText('Chargement en cours…')).toBeInTheDocument();
+    });
+  });
+
+  describe('when fetching wilders failed', () => {
     it('renders error', async () => {
       render(
-        <MockedProvider mocks={GET_WILDERS_ERROR_MOCK} addTypename={false}>
+        <MockedProvider mocks={[GET_WILDERS_ERROR_MOCK]} addTypename={false}>
           <App />
         </MockedProvider>
-        )
+      );
 
-      // const ErrMsg = await waitFor(() => screen.getByText('Erreur de chargement.'))
-      // expect(ErrMsg).toBeInTheDocument()
+      const errorMessage = await waitFor(() =>
+        screen.getByText('Erreur de chargement.')
+      );
+      expect(errorMessage).toBeInTheDocument();
+    });
+  });
 
-      await waitFor(() => screen.getByText('Erreur de chargement.'));
-      expect(screen.getByText('Erreur de chargement.')).toBeInTheDocument();
-    })
-  })
-
-  describe('while fetching wilders successed', () => {
+  describe('when fetching wilders succeeded', () => {
     it('renders wilders', async () => {
       render(
-        <MockedProvider mocks={GET_WILDERS_SUCCESS_MOCK} addTypename={false}>
+        <MockedProvider mocks={[GET_WILDERS_SUCCESS_MOCK]} addTypename={false}>
           <App />
         </MockedProvider>
       );
